412dc1da88c1ff455e2aa02d1ac5b4baca9e54ff
515hikaru <12kojima.takahiro@gmail.com>
2019-04-22 23:49:29 +0900

    change stack type

--- stack/stack.go
+++ stack/stack.go
@@ -1,20 +1,22 @@
 package stack
 
+import "github.com/515hikaru/calculate/rational"
+
 type Stack struct {
 	idx   int
-	items []string
+	items []rational.Rational
 }
 
 func NewStack() Stack {
 	return Stack{}
 }
 
-func (s *Stack) Push(x string) {
+func (s *Stack) Push(x rational.Rational) {
 	s.items = append(s.items, x)
 	s.idx += 1
 }
 
-func (s *Stack) Pop() string {
+func (s *Stack) Pop() rational.Rational {
 	s.idx -= 1
 	return s.items[s.idx]
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -1,6 +1,10 @@
 package stack
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/515hikaru/calculate/rational"
+)
 
 func TestStack(t *testing.T) {
 	s := NewStack()
@@ -14,25 +18,25 @@ func TestStack(t *testing.T) {
 
 func TestStackPush(t *testing.T) {
 	s := NewStack()
-	s.Push("foo")
-	s.Push("bar")
+	s.Push(rational.NewRational(2, 1))
+	s.Push(rational.NewRational(1, 1))
 	if s.idx != 2 {
 		t.Errorf("s.idx is expected 2. got=%d", s.idx)
 	}
-	if s.items[0] != "foo" {
-		t.Errorf("s.items[0] is expected foo, got=%v", s.items[0])
+	if s.items[0].Num != 2 && s.items[0].Dem != 1 {
+		t.Errorf("s.items[0] is expected 2, got=%v", s.items[0])
 	}
-	if s.items[1] != "bar" {
+	if s.items[1].Num != 1 && s.items[1].Dem != 1 {
 		t.Errorf("s.items[1] is expected bar, got=%v", s.items[1])
 	}
 }
 
 func TestStackPop(t *testing.T) {
 	s := NewStack()
-	s.Push("foo")
-	s.Push("bar")
+	s.Push(rational.NewRational(2, 1))
+	s.Push(rational.NewRational(1, 1))
 	v := s.Pop()
-	if v != "bar" {
+	if v.Num != 1 && v.Dem != 1 {
 		t.Errorf("v is expected bar, got=%v", v)
 	}
 	if s.idx != 1 {
