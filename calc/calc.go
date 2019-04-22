ab6f1825941af72b2712c15beb7e7649d9ade15d
515hikaru <12kojima.takahiro@gmail.com>
2019-04-23 00:31:24 +0900

    make it possible to calculation

--- calc/calc.go
+++ calc/calc.go
@@ -1,1 +1,52 @@
 package calc
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/515hikaru/calculate/parser"
+	"github.com/515hikaru/calculate/rational"
+	"github.com/515hikaru/calculate/stack"
+)
+
+func initializeStack() stack.Stack {
+	return stack.NewStack()
+}
+
+func calc(operator string, r, s rational.Rational) (rational.Rational, error) {
+	switch operator {
+	case parser.Plus:
+		return rational.Sum(r, s), nil
+	case parser.Minus:
+		return rational.Sub(r, s), nil
+	case parser.Prod:
+		return rational.Prod(r, s), nil
+	case parser.Divide:
+		return rational.Divide(r, s), nil
+	}
+	return rational.Rational{}, errors.New("Unexpected expression")
+}
+
+func Calc(tokens []parser.Token) rational.Rational {
+	st := stack.NewStack()
+	for _, tk := range tokens {
+		if tk.TokenType == parser.Operator {
+			r := st.Pop()
+			s := st.Pop()
+			t, err := calc(tk.TokenLiteral, s, r)
+			if err != nil {
+				panic(err)
+			}
+			st.Push(t)
+		} else {
+			v, err := strconv.Atoi(tk.TokenLiteral)
+			if err != nil {
+				panic(err)
+			}
+			tmp := rational.NewRational(v, 1)
+			st.Push(tmp)
+		}
+
+	}
+	return st.Pop()
+}
--- calc/calc_test.go
+++ calc/calc_test.go
@@ -1,1 +1,42 @@
 package calc
+
+import (
+	"testing"
+
+	"github.com/515hikaru/calculate/parser"
+)
+
+func TestCalc(t *testing.T) {
+	inputTokens := []parser.Token{
+		parser.Token{TokenLiteral: "1", TokenType: parser.Numeric},
+		parser.Token{TokenLiteral: "2", TokenType: parser.Numeric},
+		parser.Token{TokenLiteral: "/", TokenType: parser.Operator},
+	}
+	c := Calc(inputTokens)
+	if c.Num != 1 {
+		t.Errorf("c.Num is expected 1, got=%d", c.Num)
+	}
+	if c.Dem != 2 {
+		t.Errorf("c.Dem is expected 2, got=%d", c.Dem)
+	}
+}
+
+func TestCalcMidleExpression(t *testing.T) {
+	inputTokens := []parser.Token{
+		parser.Token{TokenLiteral: "3", TokenType: parser.Numeric},
+		parser.Token{TokenLiteral: "4", TokenType: parser.Numeric},
+		parser.Token{TokenLiteral: "+", TokenType: parser.Operator},
+		parser.Token{TokenLiteral: "1", TokenType: parser.Numeric},
+		parser.Token{TokenLiteral: "2", TokenType: parser.Numeric},
+		parser.Token{TokenLiteral: "-", TokenType: parser.Operator},
+		parser.Token{TokenLiteral: "*", TokenType: parser.Operator},
+	}
+	c := Calc(inputTokens)
+	if c.Num != -7 {
+		t.Errorf("c.Num is expected -7, got=%d", c.Num)
+	}
+	if c.Dem != 1 {
+		t.Errorf("c.Dem is expected 1, got=%d", c.Dem)
+	}
+
+}
