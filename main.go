a991e01a727e1dd1c162d152e9075879cb3a6307
515hikaru <12kojima.takahiro@gmail.com>
2019-04-23 00:31:38 +0900

    calc from std input

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/515hikaru/calculate/calc"
+	"github.com/515hikaru/calculate/parser"
+)
+
+func main() {
+	var s string
+	sc := bufio.NewScanner(os.Stdin)
+	if sc.Scan() {
+		s = sc.Text()
+	} else {
+		panic("cannot load from stdinput")
+	}
+	tokens := parser.Parse(s)
+	fmt.Printf("%v\n", tokens)
+	calc := calc.Calc(tokens)
+	fmt.Printf("%v\n", calc)
+}
